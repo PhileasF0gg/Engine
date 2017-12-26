a267acc497052bf4e13a4734e840d92027487f2c
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2017-12-27 11:43:39 +1300

    Added stone texture

--- src/map/Textures.java
+++ src/map/Textures.java
@@ -1,6 +1,7 @@
 package map;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -9,10 +10,11 @@ public class Textures {
     public static BufferedImage tileTextures[]; // Holds all of the tile textures corresponding to their ID values.
 
     public static void initTextures() {
-        tileTextures = new BufferedImage[3]; // Sets the amount of textures in use.
+        tileTextures = new BufferedImage[4]; // Sets the amount of textures in use.
         try {
             tileTextures[1] = ImageIO.read(Textures.class.getResourceAsStream("/grass.png"));
             tileTextures[2] = ImageIO.read(Textures.class.getResourceAsStream("/dirt.png"));
+            tileTextures[3] = ImageIO.read(Textures.class.getResourceAsStream("/stone.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
