851b59ef7e811ece4949834413de4736f4037ea7
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2018-01-03 15:36:15 +1300

    Added tall grass

--- src/map/Textures.java
+++ src/map/Textures.java
@@ -9,7 +9,7 @@ public class Textures {
     public static BufferedImage tileTextures[]; // Holds all of the tile textures corresponding to their ID values.
 
     public static void initTextures() {
-        tileTextures = new BufferedImage[13]; // Sets the amount of textures in use.
+        tileTextures = new BufferedImage[14]; // Sets the amount of textures in use.
         try {
             tileTextures[1] = ImageIO.read(Textures.class.getResourceAsStream("/grass.png"));
             tileTextures[2] = ImageIO.read(Textures.class.getResourceAsStream("/dirt.png"));
@@ -23,6 +23,7 @@ public class Textures {
             tileTextures[10] = ImageIO.read(Textures.class.getResourceAsStream("/dirt_b.png"));
             tileTextures[11] = ImageIO.read(Textures.class.getResourceAsStream("/stone.png"));
             tileTextures[12] = ImageIO.read(Textures.class.getResourceAsStream("/rock.png"));
+            tileTextures[13] = ImageIO.read(Textures.class.getResourceAsStream("/tall_grass.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
